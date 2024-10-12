19d1e55a04b7ac10c01cb6792ad265772b979a29
Piotr Rutkowski <rutkowski.2387@gmail.com>
2024-10-12 14:23:25 +0200

    module 1 / lesson 2

--- examples/module1/lesson2/container-components/containers/CharacterSearchContainer.tsx
+++ examples/module1/lesson2/container-components/containers/CharacterSearchContainer.tsx
@@ -1,48 +1,26 @@
-import { useEffect, useState } from 'react';
-import CharacterList from '../components/CharacterList';
-import SearchForm from '../components/SearchForm';
+import CharacterList from '../components/ListOfCharacters/CharacterList';
+import GenderInput from '../components/SearchForm/GenderInput';
+import NameInput from '../components/SearchForm/NameInput';
+import SortByInput from '../components/SearchForm/SortByInput';
 import SearchTitle from '../components/SearchTitle';
-import { Character } from '../types/Character';
+
+import useSearch from '../customHooks/useSearch';
+import useSort from '../customHooks/useSort';
 
 function CharacterSearchContainer() {
-  const [name, setName] = useState('');
-  const [gender, setGender] = useState('');
-  const [characters, setCharacters] = useState<Character[]>([]);
-  const [sortOption, setSortOption] = useState('');
-
-  useEffect(() => {
-    if (name || gender) {
-      fetch(
-        `https://rickandmortyapi.com/api/character/?name=${name}&gender=${gender}`
-      )
-        .then((response) => response.json())
-        .then((data) => setCharacters(data.results || []))
-        .catch((error) => console.error('Error fetching data:', error));
-    }
-  }, [name, gender]);
-
-  const sortedCharacters = [...characters].sort((a, b) => {
-    if (sortOption === 'name') {
-      return a.name.localeCompare(b.name);
-    } else if (sortOption === 'created') {
-      return new Date(a.created).getTime() - new Date(b.created).getTime();
-    }
-    return 0;
-  });
+  const { name, setName, setGender, gender, characters } = useSearch();
+  const { sortedCharacters, sortOption, setSortOption } = useSort(characters);
 
   return (
     <>
       <div className="pt-20" />
       <SearchTitle />
       <div className="pt-8" />
-      <SearchForm
-        name={name}
-        setName={setName}
-        gender={gender}
-        setGender={setGender}
-        sortOption={sortOption}
-        setSortOption={setSortOption}
-      />
+      <form className="space-x-4 flex items-end justify-center">
+        <NameInput value={name} setValue={setName} />
+        <GenderInput value={gender} setValue={setGender} />
+        <SortByInput value={sortOption} setValue={setSortOption} />
+      </form>
       <div className="pt-12" />
       <CharacterList characters={sortedCharacters} />
       <div className="pt-16" />
